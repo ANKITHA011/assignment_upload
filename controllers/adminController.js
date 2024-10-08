4060a525672694a1795172144d2093cd7bbf2587
ANKITHA011 <77776907+ANKITHA011@users.noreply.github.com>
2024-10-08 21:45:11 +0530

    Create  adminController.js

---
+++ controllers/adminController.js
@@ -0,0 +1,22 @@
+const User = require('../models/userModel');
+const Assignment = require('../models/assignmentModel');
+
+exports.register = async (req, res) => {
+    // Admin registration logic here
+};
+
+exports.login = async (req, res) => {
+    // Admin login logic here
+};
+
+exports.viewAssignments = async (req, res) => {
+    // View assignments logic here
+};
+
+exports.acceptAssignment = async (req, res) => {
+    // Accept assignment logic here
+};
+
+exports.rejectAssignment = async (req, res) => {
+    // Reject assignment logic here
+};

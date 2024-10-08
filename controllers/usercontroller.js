8e2c2a33149af4a4abc39c5bb4b4c9d03e36dfe8
ANKITHA011 <77776907+ANKITHA011@users.noreply.github.com>
2024-10-08 21:43:58 +0530

    Create usercontroller.js

---
+++ controllers/usercontroller.js
@@ -0,0 +1,20 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/userModel');
+const Assignment = require('../models/assignmentModel');
+
+exports.register = async (req, res) => {
+    // Register logic here
+};
+
+exports.login = async (req, res) => {
+    // Login logic here
+};
+
+exports.uploadAssignment = async (req, res) => {
+    // Upload assignment logic here
+};
+
+exports.getAllAdmins = async (req, res) => {
+    // Fetch admins logic here
+};
